9cd5180305c2bd1d19b3d81a5463a1db77f19908
yuya-nishimaki <yuya.nishimaki@excite.jp>
2020-08-30 15:12:34 +0900

    4.8

---
+++ src/ch4/4.8.ml
@@ -0,0 +1,14 @@
+(* 鶴の足の本数 *)
+let tsuru_ashi = 2
+
+(* 亀の足の本数 *)
+let kame_ashi = 4
+
+(* 目的：鶴と亀の数の合計 x と足の合計本数 y に応じて、鶴の数を計算する *)
+(* int -> int -> int *)
+let tsurukame x y = (kame_ashi * x - y) / tsuru_ashi
+
+(* テスト *)
+let test1 = tsurukame 0 0 = 0
+let test2 = tsurukame 2 6 = 1
+let test3 = tsurukame 5 16 = 2
