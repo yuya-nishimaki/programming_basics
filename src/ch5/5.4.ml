b317c84ba6c644fa273c68c919026f6731c0f047
yuya-nishimaki <yuya.nishimaki@excite.jp>
2020-09-01 08:16:43 +0900

    5.4

---
+++ src/ch5/5.4.ml
@@ -0,0 +1,8 @@
+(* 目的：二次方程式の係数 a, b, c の値に応じた、判別式の結果を返す *)
+(* hanbetsushiki: float -> float -> float -> float *)
+let hanbetsushiki a b c = b ** 2. -. 4. *. a *. c
+
+(* テスト *)
+let test1 = hanbetsushiki 0. 0. 0. = 0.
+let test2 = hanbetsushiki 1. 3. 8. = -23.
+let test3 = hanbetsushiki 2. 1. 0. = 1.
