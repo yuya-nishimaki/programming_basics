bd1c145e1d937fde808901af889eb27af2d4e6a3
yuya-nishimaki <yuya.nishimaki@excite.jp>
2020-09-01 09:14:57 +0900

    7.3

---
+++ src/ch7/7.3.ml
@@ -0,0 +1,8 @@
+(* 目的：平面座標を受け取って、x 軸について対称な点の座標を返す *)
+(* taisho_x: float * float -> float * flaot *)
+let taisho_x point = match point with (x, y) -> (x, -.y)
+
+(* テスト *)
+let test1 = taisho_x (1., 3.) = (1., -3.)
+let test2 = taisho_x (3.3, -7.2) = (3.3, 7.2)
+let test3 = taisho_x (0., 0.) = (0., 0.)
