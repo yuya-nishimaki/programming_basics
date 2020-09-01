8e9e8ac24a4f5a5e0a4ffcbdf9b163d45f3bd2a0
yuya-nishimaki <yuya.nishimaki@excite.jp>
2020-09-01 09:06:12 +0900

    7.2

---
+++ src/ch7/7.2.ml
@@ -0,0 +1,9 @@
+(* 目的：名前と成績の組から、「○○さんの評価は△です」という文字列を返す *)
+(* seiseki: string * string -> string *)
+let seiseki pair = match pair with
+    (name, score) -> name ^ "さんの評価は" ^ score ^ "です"
+
+(* テスト *)
+let test1 = seiseki ("太郎", "0") = "太郎さんの評価は0です"
+let test2 = seiseki ("ユースケ", "80") = "ユースケさんの評価は80です"
+let test3 = seiseki ("津田", "30") = "津田さんの評価は30です"
