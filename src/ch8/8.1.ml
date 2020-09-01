c94e6b3fafef518c6116aa304542d309b88393fc
yuya-nishimaki <yuya.nishimaki@excite.jp>
2020-09-02 08:47:31 +0900

    8.1

---
+++ src/ch8/8.1.ml
@@ -0,0 +1,11 @@
+type book_t = {
+    title : string;
+    author : string;
+    publisher : string;
+    price : int;
+    isbn : string;
+}
+
+let book1 = {title = "炎炎ノ消防隊(21)"; author = "大久保篤"; publisher = "講談社"; price = 495; isbn = "978-4065180631"}
+let book2 = {title = "チェンソーマン 4"; author = "藤本タツキ"; publisher = "集英社"; price = 484; isbn = "978-4088820750"}
+let book3 = {title = "D2C 「世界観」と「テクノロジー」で勝つブランド戦略"; author = "佐々木康裕"; publisher = "NewsPicksパブリッシング"; price = 2200; isbn = "978-4910063034"}
