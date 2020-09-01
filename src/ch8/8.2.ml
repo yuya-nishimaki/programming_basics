6a5e558057a469c95b86f03e1ad071fcf930e859
yuya-nishimaki <yuya.nishimaki@excite.jp>
2020-09-02 08:49:24 +0900

    8.2

---
+++ src/ch8/8.2.ml
@@ -0,0 +1,10 @@
+type okozukai_t = {
+    name : string;
+    price : int;
+    place : string;
+    date : string
+}
+
+let okozukai1 = {name = "鉛筆"; price = 100; place = "ローソン"; date = "2020/9/1"}
+let okozukai2 = {name = "漫画"; price = 500; place = "本屋"; date = "2010/5/3"}
+let okozukai3 = {name = "チョコ"; price = 250; place = "スーパー"; date = "2008/2/25"}
