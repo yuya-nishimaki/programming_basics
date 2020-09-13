e1a5bbd5a7ba5931ab58ef344be473a3967e79c3
yuya-nishimaki <yuya.nishimaki@excite.jp>
2020-09-14 02:20:39 +0900

    improve: 単位を追加

--- src/ch9/9.8.ml
+++ src/ch9/9.8.ml
@@ -1,17 +1,17 @@
 (* 人のデータ（名前、身長（m）、体重（kg）、誕生日（月と日）、血液型）を表す型 *)
 type person_t = {
     name : string; (* 名前 *)
-    height : float; (* 身長（m） *)
-    weight : float; (* 体重（kg） *)
+    height_m : float; (* 身長（m） *)
+    weight_kg : float; (* 体重（kg） *)
     birthday : int * int; (* 誕生日（月と日） *)
     blood_type : string; (* 血液型 *)
 }
 
 (* person_t 型のデータ例 *)
-let person1 = {name = "佑哉"; height = 1.68; weight = 58.; birthday = (9, 18); blood_type = "A"}
-let person2 = {name = "淳平"; height = 1.77; weight = 65.; birthday = (3, 20); blood_type = "AB"}
-let person3 = {name = "秀介"; height = 1.76; weight = 65.; birthday = (10, 5); blood_type = "O"}
-let person4 = {name = "真一"; height = 1.86; weight = 83.; birthday = (8, 23); blood_type = "O"}
+let person1 = {name = "佑哉"; height_m = 1.68; weight_kg = 58.; birthday = (9, 18); blood_type = "A"}
+let person2 = {name = "淳平"; height_m = 1.77; weight_kg = 65.; birthday = (3, 20); blood_type = "AB"}
+let person3 = {name = "秀介"; height_m = 1.76; weight_kg = 65.; birthday = (10, 5); blood_type = "O"}
+let person4 = {name = "真一"; height_m = 1.86; weight_kg = 83.; birthday = (8, 23); blood_type = "O"}
 
 (* person_t list は
     - []            空リスト、あるいは
@@ -22,11 +22,11 @@ let person4 = {name = "真一"; height = 1.86; weight = 83.; birthday = (8, 23);
 (* 8 月の最終日 *)
 let aug_last_day = 31
 
-(* 目的：person_t 型のデータリスト lst を受け取って、乙女座の人の名前のみからなるリストを返す *)
+(* 目的：person_t 型のデータリスト persons を受け取って、乙女座の人の名前のみからなるリストを返す *)
 (* otomeza : person_t list -> string list *)
-let rec otomeza lst = match lst with
+let rec otomeza persons = match persons with
     [] -> []
-    | {name = n; height = h; weight = w; birthday = (month, day); blood_type = bt} :: rest
+    | {name = n; height_m = h; weight_kg = w; birthday = (month, day); blood_type = bt} :: rest
         -> if (month = 8 && day >= 23 && day <= aug_last_day) 
             || (month = 9 && day >= 1 && day <= 22) then n :: otomeza rest
                                                     else otomeza rest
