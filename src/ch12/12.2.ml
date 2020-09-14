83d7ed70e3b49e2fb509844dbab6ff7a9b9f7e39
yuya-nishimaki <yuya.nishimaki@excite.jp>
2020-09-15 01:36:52 +0900

    improve: 変数名やスタイルを調整

--- src/ch12/12.2.ml
+++ src/ch12/12.2.ml
@@ -1,6 +1,11 @@
 # use "./metro.ml"
 # use "ch12/12.1.ml"
 
+(* ekimei_t 型のデータ例 *)
+let ekimei1 = {kanji="代々木上原"; kana="よよぎうえはら"; romaji="yoyogiuehara"; shozoku="千代田線"}
+let ekimei2 = {kanji="代々木公園"; kana="よよぎこうえん"; romaji="yoyogikouen"; shozoku="千代田線"}
+let ekimei3 = {kanji="明治神宮前"; kana="めいじじんぐうまえ"; romaji="meijijinguumae"; shozoku="千代田線"}
+
 (* ekimei_t list は
     - []            空リスト、あるいは
     - first :: rest 最初の要素が first で残りのリストが rest
@@ -8,12 +13,13 @@
     という形 *)
 
 (* ekimei_t list 型のデータ例 *)
-let ekimei_list1 = []
-let ekimei_list2 = [
-    {kanji="代々木上原"; kana="よよぎうえはら"; romaji="yoyogiuehara"; shozoku="千代田線"};
-    {kanji="代々木公園"; kana="よよぎこうえん"; romaji="yoyogikouen"; shozoku="千代田線"};
-    {kanji="明治神宮前"; kana="めいじじんぐうまえ"; romaji="meijijinguumae"; shozoku="千代田線"};
-]
+let ekimeis1 = []
+let ekimeis2 = [ekimei1; ekimei2; ekimei3]
+
+(* eki_t 型のデータ例 *)
+let eki1 = {namae = "代々木上原"; saitan_kyori = infinity; temae_list = []}
+let eki2 = {namae = "代々木公園"; saitan_kyori = infinity; temae_list = []}
+let eki3 = {namae = "明治神宮前"; saitan_kyori = infinity; temae_list = []}
 
 (* eki_t list は
     - []            空リスト、あるいは
@@ -21,17 +27,17 @@ let ekimei_list2 = [
                     （rest が自己参照のケース）
     という形 *)
 
+(* eki_t list 型のデータ例 *)
+let ekis1 = []
+let ekis2 = [eki1; eki2; eki3]
+
 (* 目的：ekimei_t 型のリストを受け取って、eki_t 型のリストを作る *)
 (* make_eki_list : ekimei_t list -> eki_t list *)
-let rec make_eki_list ekimei_list = match ekimei_list with 
+let rec make_eki_list ekimeis = match ekimeis with
     [] -> []
-    | {kanji = k1; kana = k2; romaji = r; shozoku = s} :: rest -> 
-        {namae = k1; saitan_kyori = infinity; temae_list = []} :: make_eki_list rest
+    | {kanji = kanji; kana = kana; romaji = r; shozoku = s} :: rest ->
+        {namae = kanji; saitan_kyori = infinity; temae_list = []} :: make_eki_list rest
 
 (* テスト *)
-let test1 = make_eki_list ekimei_list1 = []
-let test2 = make_eki_list ekimei_list2 = [
-    {namae = "代々木上原"; saitan_kyori = infinity; temae_list = []};
-    {namae = "代々木公園"; saitan_kyori = infinity; temae_list = []};
-    {namae = "明治神宮前"; saitan_kyori = infinity; temae_list = []};
-]
+let test1 = make_eki_list ekimeis1 = ekis1
+let test2 = make_eki_list ekimeis2 = ekis2
--- src/ch12/12.3.ml
+++ src/ch12/12.3.ml
@@ -1,38 +1,24 @@
 # use "ch12/12.2.ml"
 
-(* eki_t list は
-    - []            空リスト、あるいは
-    - first :: rest 最初の要素が first で残りのリストが rest
-                    （rest が自己参照のケース）
-    という形 *)
-
-(* eki_t list 型のデータ例 *)
-let eki_list1 = []
-let eki_list2 = [
-    {namae = "代々木上原"; saitan_kyori = infinity; temae_list = []};
-    {namae = "代々木公園"; saitan_kyori = infinity; temae_list = []};
-    {namae = "明治神宮前"; saitan_kyori = infinity; temae_list = []};
-]
-
 (* 目的：ダイクストラのアルゴリズムのステップ 1 を実行する。
         eki_t 型のリストと起点を受け取って、起点の最短距離は 0、
         駅名リストに起点の駅名のみを持つ eki_t 型のリストを返す *)
 (* shokika : eki_t list -> string -> eki_t list *)
-let rec shokika eki_list kanji_kiten = match eki_list with 
+let rec shokika ekis kanji_kiten = match ekis with
     [] -> []
     | ({namae = n; saitan_kyori = s; temae_list = t} as first) :: rest ->
         if kanji_kiten = n then {namae = n; saitan_kyori = 0.; temae_list = [n]} :: rest
         else first :: shokika rest kanji_kiten
 
 (* テスト *)
-let test1 = shokika eki_list1 "" = []
-let test2 = shokika eki_list2 "代々木上原" = [
+let test1 = shokika ekis1 "" = []
+let test2 = shokika ekis2 "代々木上原" = [
     {namae = "代々木上原"; saitan_kyori = 0.; temae_list = ["代々木上原"]};
-    {namae = "代々木公園"; saitan_kyori = infinity; temae_list = []};
-    {namae = "明治神宮前"; saitan_kyori = infinity; temae_list = []};
+    eki2;
+    eki3;
 ]
-let test3 = shokika eki_list2 "明治神宮前" = [
-    {namae = "代々木上原"; saitan_kyori = infinity; temae_list = []};
-    {namae = "代々木公園"; saitan_kyori = infinity; temae_list = []};
+let test3 = shokika ekis2 "明治神宮前" = [
+    eki1;
+    eki2;
     {namae = "明治神宮前"; saitan_kyori = 0.; temae_list = ["明治神宮前"]};
 ]
